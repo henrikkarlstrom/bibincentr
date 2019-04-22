b94e09990b94c0a6e29e3898000dd7905012f3a5
henrikkarlstrom <henrik.karlstrom@ntnu.no>
2019-04-22 21:35:30 +0200

    Update file info

--- R/get_data.R
+++ R/get_data.R
@@ -1,3 +1,17 @@
+#' Get publication data from the Cristin API
+#'
+#' Fetches authorship data for peer-reviewed academic
+#' publications with at least one Norwegian author
+#'
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+
 get_data <- function(x) {
   print(x)
 }
