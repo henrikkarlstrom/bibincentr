2e1aab0c9c1054561139b1d491a2b8943a324e11
henrikkarlstrom <henrik.karlstrom@ntnu.no>
2019-05-14 22:01:45 +0200

    document data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Time and collaboration status for academic publications.
+#'
+#' A dataset containing the ID, time registered and 
+#'
+#' @format A data frame with 61 rows and 3 variables:
+#' \describe{
+#'   \item{ID}{Numeric, unique identifier}
+#'   \item{Date}{POSIXtc, time of registration of publication}
+#'   \item{int_collab}{numeric, international collaboration 
+#'   status of publication}
+#'   }
+"data"
